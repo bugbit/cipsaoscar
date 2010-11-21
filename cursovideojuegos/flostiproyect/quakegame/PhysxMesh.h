829b9858745cd6e4e51da28eac36abdb26c2f4ca
ohernandezba <ohernandezba@71d53fa2-cca5-e1f2-4b5e-677cbd06613a>
2010-11-21 20:14:53 +0000

    Nuevas clases player,inputplayer

--- cursovideojuegos/flostiproyect/quakegame/PhysxMesh.h
+++ cursovideojuegos/flostiproyect/quakegame/PhysxMesh.h
@@ -4,15 +4,21 @@
 #include "Graphics/Object3D.h"
 
 //--Forward Declaration--
+class CRenderManager;
 class CPhysicActor;
+class CASEObject;
 
 class CPhysxMesh:public CObject3D
 {
 public:
 	CPhysxMesh																				(void);
 	~CPhysxMesh																				(void);
-	const Vect3f&																			GetPosition	() const;
+	const Vect3f&																			GetPosition();
 	void																							SetPosition(const Vect3f& pos);
+	virtual void																			Update(float elapsedTime);
+	virtual void																			RenderScene(CRenderManager* renderManager);
 private:
 	CPhysicActor																			*m_Actor;
+	CASEObject																				*m_AseObject;
+	void																							UpdatePosition();
 };
