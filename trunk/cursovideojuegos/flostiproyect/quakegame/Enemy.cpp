7b9772569f01888857b1d03261432dedb90a4b9e
ohernandezba <ohernandezba@71d53fa2-cca5-e1f2-4b5e-677cbd06613a>
2010-11-25 19:58:11 +0000

    movimiento enemy

--- trunk/cursovideojuegos/flostiproyect/quakegame/Enemy.cpp
+++ trunk/cursovideojuegos/flostiproyect/quakegame/Enemy.cpp
@@ -7,10 +7,12 @@
 #include "Math/Matrix44.h"
 
 CEnemy::CEnemy(void)
+:m_State(CEnemy::PATROL)
 {
 	for (int i=0;i<10;i++)
 	{
-		Vect3f pPos(50.f+15.f*cos(e2PIf*i/10.f),0.f,10.f+15.f*sin(e2PIf*i/10.f));
+		float alpha=e2PIf*i/10.f;
+		Vect3f pPos(35.f+15.f*cos(alpha),0.f,-5.f+15.f*sin(alpha));
 		m_WayPoints.push_back(pPos);
 	}
 	m_Position=*(m_WayPoints.begin());
@@ -23,16 +25,22 @@ CEnemy::~CEnemy(void)
 
 void CEnemy::Update(float elapsedTime)
 {
-	Vect3f dir=m_WayPoints[m_CurrentWayP]-m_Position;
-	float pLng=dir.Length();
-	dir.Normalize();
-	m_Position += dir*elapsedTime;
-	if (pLng<.5)
-		m_CurrentWayP=(m_CurrentWayP+1) % m_WayPoints.size();
+	switch(m_State)
+	{
+	case CEnemy::PATROL:
+		Patrol(elapsedTime);
+			break;
+	}
 }
 
 void CEnemy::RenderScene(CRenderManager* renderManager)
 {
+	RenderWayPoints(renderManager);
+	RenderModel(renderManager);
+}
+
+void CEnemy::RenderWayPoints(CRenderManager* renderManager)
+{	
 	std::vector<Vect3f>::iterator it=m_WayPoints.begin(),itend=m_WayPoints.end();
 
 	for (;it!=itend;it++)
@@ -52,9 +60,30 @@ void CEnemy::RenderScene(CRenderManager* renderManager)
 		pPos=*it;
 	}
 	renderManager->DrawLine(pPos,pPosIni,colCYAN);
-	Mat44f mat2;
-	mat2.SetIdentity();
-	mat2.Translate(m_Position);
-	renderManager->SetTransform(mat2);
-	renderManager->DrawCube(.8f,colGREEN);
+}
+
+void CEnemy::RenderModel(CRenderManager* renderManager)
+{
+	float ang=mathUtils::ACos(m_VectDir.x);
+	Mat44f mat;
+	mat.SetIdentity();
+	//mat.GetRotedByAngleY(ang);
+	mat.Translate(m_Position);
+	renderManager->SetTransform(mat);
+	renderManager->DrawCube(6.f,colGREEN);
+	renderManager->SetTransform((Mat44f&) m44fIDENTITY);
+	Vect3f posI=m_Position-Vect3f(3.f,3.f,3.f);
+	Vect3f posF=posI+1.5f*m_VectDir;
+	renderManager->DrawLine(m_Position,posF,colGREEN);
+}
+
+void CEnemy::Patrol(float elapsedTime)
+{
+	Vect3f dir=m_WayPoints[m_CurrentWayP]-m_Position;
+	float pLng=dir.Length();
+	dir.Normalize();
+	m_VectDir=dir;
+	m_Position += dir*elapsedTime;
+	if (pLng<.5)
+		m_CurrentWayP=(m_CurrentWayP+1) % m_WayPoints.size();
 }
